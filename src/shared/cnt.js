f4aa9a6edbdf120af086cf90667a35ee226c7460
Amit Kumar Singh <33172377+amit-kumar56@users.noreply.github.com>
2021-02-12 23:47:38 +0530

    Add files via upload

---
+++ src/shared/cnt.js
@@ -0,0 +1,36 @@
+export const CNT =
+[
+    {
+    id: 1,
+    name:'USA',
+    image: 'assets/images/us.png',
+    speed: '105',
+     distance:'12000',
+    volume:'4.99',
+    timezone:'',
+    coordinates: [37.090240, -95.712891]
+                           
+    },
+    {
+    id: 2,
+    name:'India',
+    image: 'assets/images/india.png',
+    speed: '1050',
+    distance:'120200',
+    volume:'44.99',
+    timezone:'',
+    coordinates: [20.593683, 78.962883]
+
+    },
+    {
+        id: 3,
+        name:'UK',
+        image: 'assets/images/uk.jpeg',
+       speed: '1053',
+       distance:'120050',
+       volume:'54.99',
+       timezone:'',
+       coordinates: [55.378052, -3.435973]
+    }
+    
+];
